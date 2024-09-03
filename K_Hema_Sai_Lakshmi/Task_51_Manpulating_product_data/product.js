cd1cdae17fda34419ccd739f376286f371610cb4
k_hema_sai_lakshmi <hemasailakshmi56@gmail.com>
2024-09-03 20:59:28 +0530

    added js

---
+++ K_Hema_Sai_Lakshmi/Task_51_Manpulating_product_data/product.js
@@ -0,0 +1,40 @@
+let products = [
+    { id: 1, name: "Banana", price: 1.99, category: "Food" },
+    { id: 2, name: "Shampoo", price: 7.50, category: "Personal Care" },
+    { id: 3, name: "Bread", price: 12.49, category: "Food" },
+    { id: 4, name: "Notebook", price: 13.99, category: "Stationery" },
+    { id: 5, name: "Milk", price: 1.50, category: "Food" }
+];
+function  displayProducts(array){
+    array.forEach(function(item){
+        console.log(`${item.name}-${item.price}`);
+    })
+   
+}
+let taxRate = 0.10;
+
+let productsWithTax = products.map(function(item){
+    return{
+        id:item.id,
+        name:item.name,
+        category:item.category,
+        price:(item.price * (1 + taxRate)).toFixed(2)
+
+    };
+});
+displayProducts(productsWithTax);
+let foodProducts = products.filter(function(item){
+    return item.category=="Food";
+})
+displayProducts(foodProducts);
+let AffordableProducts  = products.filter(function(item){
+    return item.price<10})
+.map(function(item) {
+     return `${item.name} - $${item.price}`
+        });
+console.log(AffordableProducts);
+totalprice=0
+products.forEach(function(item){
+   (totalprice+=item.price*(1+0.1)).toFixed(2);
+})
+console.log(`${totalprice}`);
