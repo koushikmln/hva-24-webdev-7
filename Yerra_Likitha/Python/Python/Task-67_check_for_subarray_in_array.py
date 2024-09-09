8a3470b58b8776b241293a369fbcfeb83c247748
Likhita-26 <153937574+Likhita-26@users.noreply.github.com>
2024-09-09 19:59:46 +0530

    Task-67_check_for_subarray_in_array.py

--- Yerra_Likitha/Python/Python/Task-67_check_for_subarray_in_array.py
+++ Yerra_Likitha/Python/Python/Task-67_check_for_subarray_in_array.py
@@ -1,15 +1,15 @@
-a=list(map(int,input().split()))
-b=list(map(int,input().split()))
+n=list(map(int,input().split()))
+m=list(map(int,input().split()))
 i=0
 j=0
-while i<len(a) and j<len(b):
-    if a[i]==a[j]:
+while i<len(n) and j<len(m):
+    if n[i]==m[j]:
         i+=1
         j+=1
     else:
         j=0
         i+=1
-if j==len(b):
+if j==len(m):
     print("yes")
 else:
     print("no")
