0e61b75af665b592efb6d28fefeb23eede58c0da
lokesh <lokeshtamatapu@gmail.com>
2024-10-21 21:38:09 +0530

    mini hackathon 2.0

--- Tamatapu_Lokesh/Mini-Hackathon-2.0/index.js
+++
@@ -1,4 +0,0 @@
-fetch("https://the-trivia-api.com/api/questions?limit=2")
- .then(response => response.json)
- .then(data => console.log(data))
- .catch(error => console.error(error));
---
+++ Tamatapu_Lokesh/Task 61-challenges-dom/forms.js
@@ -0,0 +1,5 @@
+let button = document.getElementById("but");
+
+const eve = button.addEventListener('click',function(){
+    console.log("Form submitted!");
+});
